70d1c31e5fd6afa6d5384de9176e80fa28022d38
Nick Evans <Dr-Evans@users.noreply.github.com>
2022-10-02 21:18:11 -0400

    Initial copy and clear buttons

--- src/pages/raid-comp/component.tsx
+++ src/pages/raid-comp/component.tsx
@@ -1,17 +1,26 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import {
   Expansion,
-  Player,
-  MAX_RAID_SIZE,
   ItemType,
+  MAX_RAID_SIZE,
+  Player,
   PlayerItem,
 } from "../../models";
 import { SpecSelection } from "./components/spec-selection";
-import { Box, Grid } from "@mui/material";
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Grid,
+  IconButton,
+  Snackbar,
+} from "@mui/material";
 import { RaidList } from "./components/raid-list";
 import { DataList } from "./components/data-list";
 import { v4 as uuidv4 } from "uuid";
 import { useDrop } from "react-dnd";
+import { FormattedMessage } from "react-intl";
+import { Close } from "@mui/icons-material";
 
 interface PublicProps {
   expansion: Expansion;
@@ -20,9 +29,15 @@ interface PublicProps {
 export type Props = PublicProps;
 
 export const RaidCompPage: React.FC<Props> = ({ expansion }) => {
+  const [showClearSnackbar, setShowClearSnackbar] = useState(false);
+  const [showCopySnackbar, setShowCopySnackbar] = useState(false);
+
   const [players, setPlayers] = useState<(Player | undefined)[]>(
     new Array(MAX_RAID_SIZE).fill(undefined)
   );
+
+  // Used for undo functionality
+  const savedPlayers = useRef<(Player | undefined)[]>([]);
 
   const [, dropRef] = useDrop(
     () => ({
@@ -124,9 +139,94 @@ export const RaidCompPage: React.FC<Props> = ({ expansion }) => {
           />
         </Box>
       </Grid>
-      <Grid xs={4} item>
-        <DataList players={players} />
+      <Grid xs={4} item container direction={"column"} spacing={2}>
+        <Grid item>
+          <ButtonGroup>
+            <Button
+              onClick={() => {
+                // TODO: copy url to clipboard
+                setShowCopySnackbar(true);
+              }}
+            >
+              <FormattedMessage
+                id={"copy-button-text"}
+                defaultMessage={"Copy (unimplemented)"}
+              />
+            </Button>
+            <Button
+              onClick={() => {
+                // For undo functionality
+                savedPlayers.current = players;
+
+                setShowClearSnackbar(true);
+                setPlayers(new Array(MAX_RAID_SIZE).fill(undefined));
+              }}
+            >
+              <FormattedMessage
+                id={"clear-button-text"}
+                defaultMessage={"Clear"}
+              />
+            </Button>
+          </ButtonGroup>
+        </Grid>
+        <Grid item xs>
+          <DataList players={players} />
+        </Grid>
       </Grid>
+      <Snackbar
+        open={showClearSnackbar}
+        autoHideDuration={6000}
+        onClose={() => setShowClearSnackbar(false)}
+        message={
+          <FormattedMessage
+            id={"snackbar-clear-body-text"}
+            defaultMessage={"Raid list cleared"}
+          />
+        }
+        action={
+          <>
+            <Button
+              color="secondary"
+              size="small"
+              onClick={() => {
+                setPlayers(savedPlayers.current);
+              }}
+            >
+              <FormattedMessage
+                id={"snackbar-clear-undo-button-text"}
+                defaultMessage={"Undo"}
+              />
+            </Button>
+            <IconButton
+              size="small"
+              color="inherit"
+              onClick={() => setShowClearSnackbar(false)}
+            >
+              <Close fontSize="small" />
+            </IconButton>
+          </>
+        }
+      />
+      <Snackbar
+        open={showCopySnackbar}
+        autoHideDuration={6000}
+        onClose={() => setShowCopySnackbar(false)}
+        message={
+          <FormattedMessage
+            id={"snackbar-copy-body-text"}
+            defaultMessage={"Link copied to clipboard"}
+          />
+        }
+        action={
+          <IconButton
+            size="small"
+            color="inherit"
+            onClick={() => setShowCopySnackbar(false)}
+          >
+            <Close fontSize="small" />
+          </IconButton>
+        }
+      />
     </Grid>
   );
 };
